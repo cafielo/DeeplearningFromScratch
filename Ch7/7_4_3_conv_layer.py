966ea281319312079e4ef49142f768add35f3f49
joonwon lee <cafielo@gmail.com>
2017-12-21 00:45:28 +0900

    add ch7 code

--- Ch7/7_4_3_conv_layer.py
+++ Ch7/7_4_3_conv_layer.py
@@ -13,3 +13,26 @@ print(col1.shape)
 x2 = np.random.rand(10,3,7,7)
 col2 = im2col(x2, 5, 5, stride=1, pad=0)
 print(col2.shape)
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = 1 + int((H + 2*self.pad - FH) / self.stride)
+        out_w = 1 + int((W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+
+        out = np.dot(col, col_W) + self.b
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+        
+        return out
+
--- Ch7/7_5_SimpleConvNet.py
+++ Ch7/7_5_SimpleConvNet.py
@@ -6,87 +6,85 @@ import numpy as np
 from common.util import im2col
 
 
-# class SimpleConvNet:
-#     """ConvNet
-#     conv - relu - pool - affine - relu - affine - softmax
+class SimpleConvNet:
+    """ConvNet
+    conv - relu - pool - affine - relu - affine - softmax
     
-#     Parameters
-#     ----------
-#     input_size 
-#     hidden_size_list
-#     output_size 
-#     activation : 'relu' or 'sigmoid'
-#     weight_init_std : （e.g. 0.01）
-#     """
-#     def __init__(self, input_dim=(1, 28, 28), 
-#                  conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
-#                  hidden_size=100, output_size=10, weight_init_std=0.01):
+    Parameters
+    ----------
+    input_size 
+    hidden_size_list
+    output_size 
+    activation : 'relu' or 'sigmoid'
+    weight_init_std : （e.g. 0.01）
+    """
+    def __init__(self, input_dim=(1, 28, 28), 
+                 conv_param={'filter_num':30, 'filter_size':5, 'pad':0, 'stride':1},
+                 hidden_size=100, output_size=10, weight_init_std=0.01):
         
-#         filter_num = conv_param['filter_num']
-#         filter_size = conv_param['filter_size']
-#         filter_pad = conv_param['pad']
-#         filter_stride = conv_param['stride']
-#         input_size = input_dim[1]
-#         conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
-#         pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_pad = conv_param['pad']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size - filter_size + 2*filter_pad) / filter_stride + 1
+        pool_output_size = int(filter_num * (conv_output_size/2) * (conv_output_size/2))
 
-#         # 가중치 초기화
-#         self.params = {}
-#         self.params['W1'] = weight_init_std * \
-#                             np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
-#         self.params['b1'] = np.zeros(filter_num)
-#         self.params['W2'] = weight_init_std * \
-#                             np.random.randn(pool_output_size, hidden_size)
-#         self.params['b2'] = np.zeros(hidden_size)
-#         self.params['W3'] = weight_init_std * \
-#                             np.random.randn(hidden_size, output_size)
-#         self.params['b3'] = np.zeros(output_size)
+        # 가중치 초기화
+        self.params = {}
+        self.params['W1'] = weight_init_std * \
+                            np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        self.params['b1'] = np.zeros(filter_num)
+        self.params['W2'] = weight_init_std * \
+                            np.random.randn(pool_output_size, hidden_size)
+        self.params['b2'] = np.zeros(hidden_size)
+        self.params['W3'] = weight_init_std * \
+                            np.random.randn(hidden_size, output_size)
+        self.params['b3'] = np.zeros(output_size)
 
-#         # CNN  구성하는 계층 생성
-#         self.layers = OrderedDict()
-#         self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
-#                                            conv_param['stride'], conv_param['pad'])
-#         self.layers['Relu1'] = Relu()
-#         self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
-#         self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
-#         self.layers['Relu2'] = Relu()
-#         self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
+        # CNN  구성하는 계층 생성
+        self.layers = OrderedDict()
+        self.layers['Conv1'] = Convolution(self.params['W1'], self.params['b1'],
+                                           conv_param['stride'], conv_param['pad'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Pool1'] = Pooling(pool_h=2, pool_w=2, stride=2)
+        self.layers['Affine1'] = Affine(self.params['W2'], self.params['b2'])
+        self.layers['Relu2'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W3'], self.params['b3'])
 
-#         self.last_layer = SoftmaxWithLoss()
+        self.last_layer = SoftmaxWithLoss()
 
 
-#     def predict(self, x):
-#         for layer in self.layers.values():
-#             x = layer.forward(x)
-#         return x
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
+        return x
 
-#     def loss(self, x, t):
-#         y = self.predict(x)
-#         return self.last_layer.forward(y, t)
-
-    
-#     # 오차역전파법으로 기울기 구하기 
-#     def gradient(self, x, t):
-#         # forward
-#         self.loss(x, t)
-
-#         # backward
-#         dout = 1
-#         dout = self.last_layer.backward(dout)
-
-#         layers = list(self.layers.values())
-#         layers.reverse()
-#         for layer in layers:
-#             dout = layer.backward(dout)
-
-#         # 결과 저장
-#         grads = {}
-#         grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
-#         grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
-#         grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
-
-#         return grads
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.last_layer.forward(y, t)
 
     
+    # 오차역전파법으로 기울기 구하기 
+    def gradient(self, x, t):
+        # forward
+        self.loss(x, t)
 
-print sys.version
+        # backward
+        dout = 1
+        dout = self.last_layer.backward(dout)
+
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            dout = layer.backward(dout)
+
+        # 결과 저장
+        grads = {}
+        grads['W1'], grads['b1'] = self.layers['Conv1'].dW, self.layers['Conv1'].db
+        grads['W2'], grads['b2'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+        grads['W3'], grads['b3'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
+
+        return grads
+
+    
